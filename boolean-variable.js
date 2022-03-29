e247237f44917d6c388aa739ec00356aa0bf9841
jcaoo <98046622+jcaoo@users.noreply.github.com>
2022-03-29 10:01:42 -0400

    Add files via upload

---
+++ boolean-variable.js
@@ -0,0 +1,10 @@
+var inStock;
+var shipping;
+instock = true;
+shipping = false;
+
+var elStock = document.getElementById('stock');
+elStock.className = inStock;
+
+var elShip = document.getElementById('shipping');
+elShip.className = shipping;
---
+++ string-variable.js
@@ -0,0 +1,9 @@
+var username;
+var message;
+username = 'Molly' 
+message = 'See our upcoming range';
+
+var elName = document.GetElementById('name');
+elName.textContent = username;
+var elNote = document.GetElementById('note');
+elNote.textContent = message;
